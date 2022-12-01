0ea2409aaebed75cc504193622ea3a4b81af1026
Taylor Fausak <taylor@fausak.me>
2022-12-01 09:34:46 -0600

    Solve day 1 part 1 (#6)
    * Solve day 1 part 1
    
    * Fix linter
    
    Should have been done in #5.

--- source/Program.fs
+++ source/Program.fs
@@ -1,2 +1,13 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+exception UnknownArgumentsException of string[]
+
+[<EntryPoint>]
+let main (arguments: string[]) : int =
+    let input = stdin.ReadToEnd()
+
+    let output =
+        match arguments with
+        | [| "1"; "1" |] -> Day01.Part1.solve input
+        | _ -> raise (UnknownArgumentsException arguments)
+
+    printfn $"%s{output}"
+    0
