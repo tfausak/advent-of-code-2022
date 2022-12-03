c5bda3501bb220d97e70c8d1fee30c89e1b5959c
Taylor Fausak <taylor@fausak.me>
2022-12-03 13:59:13 +0000

    Refactor

--- AdventOfCode.Library/Day03/Part1.fs
+++ AdventOfCode.Library/Day03/Part1.fs
@@ -1,18 +1,17 @@
 module AdventOfCode.Library.Day03.Part1
 
-let solve (input : string) : string =
+let solve (input: string) : string =
     input.Split "\n"
     |> Seq.filter (System.String.IsNullOrWhiteSpace >> not)
-    |> Seq.map (fun line ->
+    |> Seq.collect (fun line ->
         let mid = line.Length / 2
-        (line.Substring (0, mid), line.Substring mid))
-    |> Seq.map (fun (x, y) -> (Set.ofSeq x, Set.ofSeq y))
-    |> Seq.map (fun (x, y) -> Set.intersect x y)
-    |> Seq.map Set.toSeq
-    |> Seq.concat
-    |> Seq.map (fun x ->
-        if System.Char.IsUpper x
-        then int x - int 'A' + 27
-        else int x - int 'a' + 1)
-    |> Seq.sum
+        let first = line.Substring(0, mid)
+        let second = line.Substring mid
+        let both = Set.intersect (Set.ofSeq first) (Set.ofSeq second)
+        Set.toSeq both)
+    |> Seq.sumBy (fun x ->
+        if System.Char.IsUpper x then
+            int x - int 'A' + 27
+        else
+            int x - int 'a' + 1)
     |> string
