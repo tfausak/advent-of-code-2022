f55043d6943f164acfbacfc2d65a7be17efe8ac2
Taylor Fausak <taylor@fausak.me>
2022-12-03 14:24:33 +0000

    Refactor

--- AdventOfCode.Library/Day03/Part2.fs
+++ AdventOfCode.Library/Day03/Part2.fs
@@ -1,10 +1,6 @@
 module AdventOfCode.Library.Day03.Part2
 
-let priority (x: char) : int =
-    if System.Char.IsUpper x then
-        int x - int 'A' + 27
-    else
-        int x - int 'a' + 1
+open AdventOfCode.Library.Day03.Part1
 
 let solve (input: string) : string =
     input.Split "\n"
