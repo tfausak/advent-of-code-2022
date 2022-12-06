c29ac31fc5a6db5fec39c414201f71e612b80511
Taylor Fausak <taylor@fausak.me>
2022-12-06 00:18:33 +0000

    Refactor

--- AdventOfCode.Library/Day05/Part2.fs
+++ AdventOfCode.Library/Day05/Part2.fs
@@ -1,27 +1,11 @@
 module AdventOfCode.Library.Day05.Part2
 
 open AdventOfCode.Library.Common
-
-let skipLast (source: 'T seq) : 'T seq = Seq.take (Seq.length source - 1) source
+open AdventOfCode.Library.Day05.Part1
 
 let solve (input: string) : string =
-    let (rawStacks, rawProcedure) = input.Split "\n\n" |> seqToTuple |> Option.get
-
-    let stacks =
-        rawStacks.Split "\n"
-        |> Seq.map (Seq.chunkBySize 4 >> Seq.map (Seq.item 1))
-        |> Seq.transpose
-        |> Seq.map (Seq.filter (fun crate -> crate <> ' ') >> skipLast >> Seq.toList)
-        |> Seq.toArray
-
-    let procedure =
-        rawProcedure.Split "\n"
-        |> Seq.filter (fun line -> line <> "")
-        |> Seq.map (fun line ->
-            match line.Split " " with
-            | [| "move"; count; "from"; source; "to"; target |] -> (int count, int source - 1, int target - 1)
-            | step -> failwith $"invalid step: %A{step}")
-
+    let (stacks, procedure) = parseInput input
+    
     for (count, source, target) in procedure do
         let (before, after) = List.splitAt count stacks[source]
         stacks[source] <- after
