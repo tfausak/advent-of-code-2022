0a17da3fc601794a2cb857647de0b1ecd08c6398
Taylor Fausak <taylor@fausak.me>
2022-12-07 17:15:28 +0000

    Refactor

--- AdventOfCode.Library/Day07/Part1.fs
+++ AdventOfCode.Library/Day07/Part1.fs
@@ -6,6 +6,29 @@ type Action =
     | Directory of string
     | File of int * string
 
+let getFiles (input: string) : (string list * (string * int)) list =
+    input.TrimEnd().Split "\n"
+    |> Seq.map (fun line ->
+        match line.Split " " with
+        | [| "$"; "cd"; name |] -> ChangeDirectory name
+        | [| "$"; "ls" |] -> List
+        | [| "dir"; name |] -> Directory name
+        | [| size; name |] -> File(int size, name)
+        | _ -> failwith $"invalid line: %A{line}")
+    |> Seq.fold
+        (fun (path, files) action ->
+            match action with
+            | ChangeDirectory name ->
+                match name with
+                | "/" -> ([], files)
+                | ".." -> (List.tail path, files)
+                | _ -> (name :: path, files)
+            | List -> (path, files)
+            | Directory _ -> (path, files)
+            | File(size, name) -> (path, (List.rev path, (name, size)) :: files))
+        ([], [])
+    |> snd
+
 let rec hasPrefix (prefix: 'T list) (list: 'T list) : bool =
     match (prefix, list) with
     | ([], _) -> true
@@ -17,39 +40,15 @@ let rec prefixes (list: 'T list) : 'T list list =
     | [] -> []
     | h :: t -> [ h ] :: List.map (fun x -> h :: x) (prefixes t)
 
-let solve (input: string) =
-    let allFiles =
-        input.TrimEnd().Split "\n"
-        |> Seq.map (fun line ->
-            match line.Split " " with
-            | [| "$"; "cd"; name |] -> ChangeDirectory name
-            | [| "$"; "ls" |] -> List
-            | [| "dir"; name |] -> Directory name
-            | [| size; name |] -> File(int size, name)
-            | _ -> failwith $"invalid line: %A{line}")
-        |> Seq.fold
-            (fun (path, files) action ->
-                match action with
-                | ChangeDirectory name ->
-                    match name with
-                    | "/" -> ([], files)
-                    | ".." -> (List.tail path, files)
-                    | _ -> (name :: path, files)
-                | List -> (path, files)
-                | Directory _ -> (path, files)
-                | File(size, name) -> (path, (List.rev path, (name, size)) :: files))
-            ([], [])
-        |> snd
-
-    allFiles
+let getSizes (files: (string list * (string * int)) seq) : int seq =
+    files
     |> Seq.map fst
     |> Seq.collect prefixes
     |> Set.ofSeq
-    |> Seq.map (fun path ->
-        allFiles
-        |> Seq.filter (fst >> hasPrefix path)
-        |> Seq.sumBy (snd >> snd)
-        |> fun size -> (path, size))
-    |> Seq.filter (fun (_, size) -> size <= 100000)
-    |> Seq.sumBy snd
+    |> Seq.map (fun path -> files |> Seq.filter (fst >> hasPrefix path) |> Seq.sumBy (snd >> snd))
+
+let solve (input: string) =
+    getSizes (getFiles input)
+    |> Seq.filter (fun size -> size <= 100000)
+    |> Seq.sum
     |> string
